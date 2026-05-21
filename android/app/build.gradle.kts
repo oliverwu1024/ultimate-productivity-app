38f950905870649d8cdcaae7d4ea02bf46ea3418
olivermsi <oliverwu1024@users.noreply.github.com>
2026-05-21 13:05:41 +1000

    ship apk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.ultiq.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 18
-        versionName = "2.3"
+        versionCode = 19
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
